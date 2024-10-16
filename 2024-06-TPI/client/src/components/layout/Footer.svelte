65df5cc163bb6a0efe0e5f5da4ffd85d1885128f
Max <maxcocito@gmail.com>
2024-10-16 03:34:49 -0300

    privacidad

---
+++ 2024-06-TPI/client/src/components/layout/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left" style="background: linear-gradient(to bottom, rgb(109, 46, 1), rgba(109, 46, 1, 0.8));">
+  <div class="bg-black/5 p-4 text-center text-surface dark:text-white">
+    © 2024 UTN_FRRE Grupo 6
+  </div>
+</footer>
---
+++ 2024-06-TPI/client/src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+
+<!-- src/routes/+layout.svelte -->
+<script>
+  let logo = "https://raw.githubusercontent.com/FRRe-DS/2024-06-TPI/main/client/src/public/bienal_logo_corto.png";
+  let anio= 2024;
+  import Navbar from '../components/layout/Navbar.svelte';
+  import Footer from '../components/layout/Footer.svelte';
+</script>
+
+<svelte:head>
+  <title>Bienal 2024 {anio}</title>
+  <link rel="icon" type="image/x-icon" href={logo} style="max-height: 16px; max-width: 16px" class="bg-" />
+</svelte:head>
+
+<main>
+
+  <Navbar />
+  
+  <slot></slot> <!-- Aquí se renderizarán las páginas hijas -->
+  
+  <Footer />
+
+</main>
+
+<style>
+  /* Agrega estilos generales aquí */
+  
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+
+:global(body) {
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
